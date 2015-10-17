1107802a77faed7b62422f759b812a3d4407bfa2
neoeahit <neoeahit@gmail.com>
2015-10-17 16:05:16 -0400

    #2 user auth modal

--- static/js/app.js
+++ static/js/app.js
@@ -1,41 +1,65 @@
 'use strict';
 
-var activityModule = angular.module('routerApp', ['angular-svg-round-progress','ngRoute','ui.select','ui.router']);
-
-activityModule.config(function($stateProvider, $urlRouterProvider) {
+var activityModule = angular.module('routerApp', ['angular-svg-round-progress', 'ui.bootstrap','ngRoute', 'ui.select', 'ui.router']);
+activityModule.config(function ($stateProvider, $urlRouterProvider) {
 
     $urlRouterProvider.otherwise('/home');
 
     $stateProvider
-
         .state('home', {
             url: '/home',
-            templateUrl: 'partials/home.html'
+            templateUrl: 'partials/home.html',
+            data: {
+                requireLogin: false
+            }
         })
-
+        .state('login', {
+            url: '/login',
+            templateUrl: 'partials/login.html',
+            controller: 'LoginCtrl',
+            data: {
+                requireLogin: false
+            }
+        })
         .state('reviews', {
             url: '/reviews',
             templateUrl: 'partials/reviews.html',
-            controller: 'DemoCtrl'
-
+            controller: 'DemoCtrl',
+            data: {
+                requireLogin: true
+            }
         })
-
         .state('about', {
             url: '/about',
             templateUrl: 'partials/about.html',
-            controller: 'placesCtrl'
+            controller: 'placesCtrl',
+            data: {
+                requireLogin: false
+            }
 
         })
-
-        .state('addblog', {
-            url: '/addblog',
-            templateUrl: 'partials/addblogs.html'
-        })
-        .state('race', {
-            url: '/stories/:id',
-            templateUrl: 'partials/specificRace.html',
-            controller: 'race'
-        });
 
 
 });
+
+activityModule.run(function ($rootScope, $state, LoginService) {
+
+    $rootScope.$on('$stateChangeStart', function (event, toState, toParams) {
+        var requireLogin = toState.data.requireLogin;
+        console.log($rootScope.currentUser)
+        if (requireLogin && typeof $rootScope.currentUser === 'undefined') {
+            event.preventDefault();
+
+            LoginService()
+                .then(function () {
+                    return $state.go(toState.name, toParams);
+                })
+                .catch(function () {
+                    return $state.go('home');
+                });
+        }
+    });
+
+});
+
+
---
+++ static/js/loginCtrl.js
@@ -0,0 +1,36 @@
+activityModule.service('LoginService', function ($modal, $rootScope) {
+
+    function assignCurrentUser(user) {
+        $rootScope.currentUser = user;
+        return user;
+    }
+
+    return function () {
+        var instance = $modal.open({
+            templateUrl: 'partials/login.html',
+            controller: 'LoginCtrl'
+        })
+
+        return instance.result.then(assignCurrentUser);
+    };
+
+}).controller('LoginCtrl', function ($http, $scope, $rootScope) {
+    $scope.login = function () {
+        $http({
+            url: '/auth/login/',
+            method: 'POST',
+            data: {
+                'username': $scope.username,
+                'password': $scope.password
+            }
+        }).then(function (response) {
+            if(response.data.status == 'SUCCESS')
+            {
+                $rootScope.currentUser = $scope.username;
+                $scope.$close($rootScope.currentUser)
+            }
+        }, function (error) {
+            alert(error.data);
+        })
+    };
+});
