0e3cbc28a3e8aa000162749588e29d41b7962a9b
neoeahit <neoeahit@gmail.com>
2015-10-18 00:30:01 -0400

    #3 adding a class to take care of adding reviews, this also contains all metods converted to post

--- powerhub/manager.py
+++ powerhub/manager.py
@@ -7,6 +7,7 @@ from bson.objectid import ObjectId
 from tornado.options import options
 from instagram import client
 import config
+import json
 
 api = client.InstagramAPI(access_token=config.access_token, client_secret=config.client_secret)
 
@@ -15,29 +16,53 @@ class UpdateUpvotes(auth.BaseHandler):
     @tornado.web.asynchronous
     @gen.engine
     @tornado.web.authenticated
-    def get(self, reviewId):
+    def post(self):
+        request_params = json.loads(self.request.body)
+        reviewId = request_params['id']
         cursor = yield db.reviews.update({"_id": ObjectId(str(reviewId))}, {
             '$inc': {
                 'upvote': 1
             }
         })
         if (cursor["updatedExisting"] == True):
-            self.write({"status": "successful"})
+            self.write({"status": "SUCCESS"})
         else:
-            self.write({"status": "failure"})
+            self.write({"status": "FAILURE"})
         self.finish()
+
+
+class AddReview(auth.BaseHandler):
+    @tornado.web.asynchronous
+    @gen.engine
+    @tornado.web.authenticated
+    def post(self):
+        request_params = json.loads(self.request.body)
+        race_name = request_params['race']
+        pros = request_params['pros']
+        cons = request_params['cons']
+        story = request_params['story']
+        rating = request_params['rating']
+        cursor = yield db.reviews.insert(
+            {"upvote":0, "race": race_name, "rating": rating, "pros": pros, "cons": cons, "story": story})
+        if cursor:
+            self.write({"status": "SUCCESS"})
+        else:
+            self.write({"status": "FAILURE"})
+        self.finish()
+
 
 
 class RaceReviewHandler(auth.BaseHandler):
     @tornado.web.asynchronous
     @gen.engine
     @tornado.web.authenticated
-    def get(self, race_name):
+    def post(self):
+        request_params = json.loads(self.request.body)
+        race_name = request_params['race']
         images = []
         response = api.tag_recent_media(tag_name=str(race_name))
         for media in response[0]:
             images.append(media.get_standard_resolution_url())
-
         cursor = yield db.raceinfo.find_one({"name": race_name})
         if cursor:
             race_info = {"name": str(cursor["name"]), "location": str(cursor["location"]), "date": str(cursor["date"]),
@@ -48,11 +73,11 @@ class RaceReviewHandler(auth.BaseHandler):
                 obj = people_review.next_object()
                 reviews.append(
                     {"id": str(obj["_id"]),
-                     "rating": {"cost": str(obj["rating"]["cost"]),
-                                "course": str(obj["rating"]["course"]),
-                                "fluid": str(obj["rating"]["fluid"]),
-                                "fuel": str(obj["rating"]["fuel"]),
-                                "crowd": str(obj["rating"]["crowd"])
+                     "rating": {"cost": str(obj["rating"]["Cost"]),
+                                "course": str(obj["rating"]["Course"]),
+                                "fluid": str(obj["rating"]["Fluid"]),
+                                "fuel": str(obj["rating"]["Fuel"]),
+                                "crowd": str(obj["rating"]["Crowd"])
                      },
                      "pros": str(obj["pros"]),
                      "cons": str(obj["cons"]),
@@ -70,8 +95,9 @@ class RaceReviewHandler(auth.BaseHandler):
 
 if __name__ == "__main__":
     handlers = [
-        (r"/api/race/(.*)", RaceReviewHandler),
-        (r"/api/update/upvotes/(.*)", UpdateUpvotes),
+        (r"/api/race", RaceReviewHandler),
+        (r"/api/update/upvotes", UpdateUpvotes),
+        (r"/api/add/review", AddReview),
         (r"/auth/login/", auth.AuthLoginHandler),
         (r"/auth/logout/", auth.AuthLogoutHandler),
         (r"/(.*)", tornado.web.StaticFileHandler, {"path": "static", "default_filename": "index.html"}
